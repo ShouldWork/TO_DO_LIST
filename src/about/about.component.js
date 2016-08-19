01daa92c1b89685ad2e06b1568513045495fbadd
ShouldWork <mrblocke@gmail.com>
2016-08-19 17:30:23 -0600

    update

--- src/about/about.component.js
+++ src/about/about.component.js
@@ -1,7 +1,7 @@
 (function() {
     angular.module("toDoApp")
         .component('aboutPage', {
-            templateUrl: "about/about.component.html"
+            templateUrl: "src/about/about.component.html"
         })
         .config(function($stateProvider){
             $stateProvider.state("about-page",{
